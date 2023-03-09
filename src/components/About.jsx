ec2a68d87e38e22e262fa89d7e6159349edbb45c
Chris Zavala <frappejcat@gmail.com>
2023-03-09 09:33:52 -0600

    adding some corrective text from my about section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,20 +14,18 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-          I am passionate about building excellent software full stack applications that improves
+          I am passionate about building excellent full stack applications that improve
           the lives of those around me. I am thoughtful and extremely diligent about the projects that I create.
-          I also have an electrical engineering degree, so I have a vastly diverse technical background.
+          I also have an electrical engineering degree, so I have a diverse technical background.
         </p>
 
         <br />
 
         <p className="text-xl">
-          My goal from this journey is to refine my craft and skill in this coding bootcamp to become a full-stack web developer
-          with the skills necessary to enjoy my new career but also bring the skills that I have learn through all my life experiences
-          and bring them altogether in one big broad stroke. I have a degree a Bachelor’s of Science in Electrical Engineering and I also
-          have a love for the engineering process and solve problems. With this brief introduction that I have told you, I hope to have given
-          you a little understanding about what I like to do and love to create. I hope you enjoy this site as much as I have enjoyed the
-          process of bringing it to life! Thank you.
+          My goal from this journey is to refine my craft and skills, from The University of Texas at Austin coding boot camp that I have been in for the past
+          three months, with a desire to become a full stack web developer. I have a Bachelors of Science in Electrical Engineering and I
+          have a love for the engineering process and solving problems. I hope to have given you a little understanding about what I like to do and love to create.
+          I hope you enjoy this site as much as I have enjoyed the process of bringing it to life!
         </p>
       </div>
     </div>
