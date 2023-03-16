18fec41c6f17d0715d99a0e2f84c62855610e254
Chris Zavala <frappejcat@gmail.com>
2023-03-16 13:45:50 -0500

    added the project 3 to the portfolio.

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@ import ProjIm from '../assets/portfolio/Blog.jpg';
 import ProjIm2 from '../assets/portfolio/Cineme.jpg';
 import ProjIm3 from '../assets/portfolio/DrinksRUs.png';
 import ProjIm4 from '../assets/portfolio/Trailer.jpg';
+import ProjIm5 from '../assets/portfolio/PlanTracker.jpg';
 
 export const data=[
     {
@@ -20,13 +21,21 @@ export const data=[
     },
     {
         id:3,
+        name:"PlanTracker Full Stack Web App",
+        image:ProjIm5,
+        github:"https://github.com/kpeazzoni/plan-tracker-app",
+        live:"https://plan-tracker-app.herokuapp.com/",
+    },
+
+    {
+        id:4,
         name:"The MVC Tech Blog Full Stack Web App",
         image:ProjIm,
         github:"https://github.com/ChrisZavala/UTA-bootcamp-challenge14",
         live:"https://polar-castle-47001.herokuapp.com/",
     },
     {
-        id:4,
+        id:5,
         name:"CineMe Trailer React App",
         image:ProjIm4,
         github:"",
